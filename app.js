9a699466d517316e8edcfddfb7b2af1b4f207861
sspcloud1610 <sspcloud1610.com>
2024-06-24 01:26:10 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("h1", {id : 'heading'}, "Hii Earth");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
